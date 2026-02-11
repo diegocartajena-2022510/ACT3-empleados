143922ceda18566964ed3472eb8967f324721282
diegoCartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-02-11 11:25:00 -0600

    feat: se realizo crud de ventas

--- proyecto1/src/main/java/com/diegocartajena/proyecto1/repository/VentasRepository.java
+++ proyecto1/src/main/java/com/diegocartajena/proyecto1/repository/VentasRepository.java
@@ -1,9 +1,4 @@
 package com.diegocartajena.proyecto1.repository;
 
-import com.diegocartajena.proyecto1.entity.Ventas;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VentasRepository extends JpaRepository<Ventas, Integer> {
+public interface VentasRepository {
 }
--- proyecto1/src/main/java/com/diegocartajena/proyecto1/service/VentasService.java
+++ proyecto1/src/main/java/com/diegocartajena/proyecto1/service/VentasService.java
@@ -1,15 +1,4 @@
 package com.diegocartajena.proyecto1.service;
 
-import com.diegocartajena.proyecto1.entity.Ventas;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
 public interface VentasService {
-    List<Ventas> getAllVentas();
-    Ventas getVentasById(Integer id);
-    Ventas saveVentas(Ventas ventas) throws RuntimeException;
-    Ventas updateVentas(Integer id,Ventas ventas);
-    void deleteVentas (Integer id);
 }
