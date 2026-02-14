6192b746dd4637d9c6a1741cca54a294f0454c1f
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-02-13 20:34:11 -0600

    feat: se agregaron validaciones

--- proyecto1/src/main/java/com/diegocartajena/proyecto1/entity/Proveedores.java
+++ proyecto1/src/main/java/com/diegocartajena/proyecto1/entity/Proveedores.java
@@ -1,6 +1,10 @@
 package com.diegocartajena.proyecto1.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 @Entity
 @Table(name="Proveedores")
@@ -11,15 +15,22 @@ public class Proveedores {
     @Column(name = "idProveedor")
     private Integer idProveedor;
 
+    @NotBlank(message = "el nombre es obligatorio")
+    @Pattern(regexp ="^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]*$", message = "El nombre no puede contener numeros ni caracteres especiales")
     @Column(name = "nombreProveedor")
     private String nombreProveedor;
 
+    @NotNull(message = "este campo es obligatorio")
+    @Min(value=1,message = "el numero telefonico no es valido")
     @Column(name = "telefonoProveedor")
     private Integer telefonoProveedor;
 
+    @NotBlank(message = "la direccion es obligatoria")
     @Column(name = "direccionProveedor")
     private String direccionProveedor;
 
+    @NotBlank(message = "el email es obligatorio")
+    @Pattern(regexp="^[a-zA-Z0-9._%+-]+@gmail\\.com$", message="el formato del correo no es valido")
     @Column(name = "emailProveedor")
     private String emailProveedor;
 
