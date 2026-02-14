5b0c20f523b247b8329b95466f4fcca7cb1d0810
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-02-13 20:34:41 -0600

    feat: se agregaron validaciones

--- proyecto1/src/main/java/com/diegocartajena/proyecto1/controller/RepuestosController.java
+++ proyecto1/src/main/java/com/diegocartajena/proyecto1/controller/RepuestosController.java
@@ -6,9 +6,13 @@ import com.diegocartajena.proyecto1.service.RepuestosService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/repuestos")
@@ -66,5 +70,17 @@ public class RepuestosController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
 
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException e){
+        Map<String,String> errores= new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName= ((FieldError) error).getField();
+            String errorMessage=error.getDefaultMessage();
+            errores.put(fieldName,errorMessage);
+        });
+        return errores;
     }
 }
--- proyecto1/src/main/java/com/diegocartajena/proyecto1/entity/Repuestos.java
+++ proyecto1/src/main/java/com/diegocartajena/proyecto1/entity/Repuestos.java
@@ -1,25 +1,37 @@
 package com.diegocartajena.proyecto1.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import org.aspectj.bridge.Message;
 
 @Entity
 @Table(name = "Repuestos")
 public class Repuestos {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     @Column(name = "idRepuesto")
     private Integer idRepuesto;
 
+    @NotBlank(message = "el nombre es obligatorio")
+    @Pattern(regexp ="^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]*$", message = "El nombre no puede contener numeros ni caracteres especiales")
     @Column(name = "nombreRepuesto")
     private String nombreRepuesto;
 
+    @NotBlank(message = "la categoria es obligatoria")
+    @Pattern(regexp ="^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]*$", message = "la categoria no puede contener numeros ni caracteres especiales")
     @Column(name = "categoriaRepuesto")
     private String categoriaRepuesto;
 
+    @NotNull(message = "este campo es obligatorio")
+    @Min(value=1,message = "el precio debe ser mayor a 1")
     @Column(name = "precioCompra")
     private double precioCompra;
 
+    @NotNull(message = "este campo es obligatorio")
+    @Min(value=1,message = "el precio debe ser mayor a 1")
     @Column(name = "precioVenta")
     private double precioVenta;
 
